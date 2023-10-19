04cfb4c37a846b4bb598ad86cb31b1119313fa35
Abishek <abishekhari09@gmail.com>
2023-10-19 16:33:32 +0530

    updated home

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Components";
+import { Header } from "../components/components";
 
 const HomeLayout = () => {
   return (
